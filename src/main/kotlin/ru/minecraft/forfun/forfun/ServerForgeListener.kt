b8ecb773de2a408c810b11f12c48fe58d226f36b
cisster <95279069+cisster@users.noreply.github.com>
2025-10-05 13:14:39 +0300

    AutoUpdater update

--- src/main/kotlin/ru/minecraft/forfun/forfun/ServerForgeListener.kt
+++ src/main/kotlin/ru/minecraft/forfun/forfun/ServerForgeListener.kt
@@ -4,13 +4,13 @@ import net.minecraftforge.event.TickEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import ru.minecraft.forfun.forfun.util.autoupdater.AutoUpdater
 
-object ServerForgeListener {
-    var Checked = false
-    @SubscribeEvent
-    fun onServerTick(event: TickEvent.ServerTickEvent) {
-        if (!AutoUpdater.checkShowScreen() && !Checked) {
-            AutoUpdater.downloadUpdate()
-            Checked = true
-        }
-    }
-}
+//object ServerForgeListener {
+//    var Checked = false
+//    @SubscribeEvent
+//    fun onServerTick(event: TickEvent.ServerTickEvent) {
+//        if (!AutoUpdater.checkShowScreen() && !Checked) {
+//            AutoUpdater.downloadUpdate()
+//            Checked = true
+//        }
+//    }
+//}
